e45e57e8db3da5c92aad0dcb62d92d43a067cd86
hackthacker <nirajadhiakri572@gmail.com>
2023-09-26 14:08:31 +0545

    new added javascript

---
+++ learn js/2. important function.js
@@ -0,0 +1,5 @@
+alert("hello this is alert tags");
+confirm(" plz conform yes or no");
+console.log("hello prints")
+document.write("this is doucuments. write");
+prompt("hello this is prompt","defaults");
---
+++ learn js/3. variables.js
@@ -0,0 +1,13 @@
+//  Declare and initialize variables
+
+        // Using var
+        var varExample = "I am a variable declared with var.";
+        document.getElementById("var").textContent = varExample;
+
+        // Using let
+        let letExample = "I am a variable declared with let.";
+        document.getElementById("let").textContent = letExample;
+
+        // Using const
+        const constExample = "I am a variable declared with const.";
+        document.querySelector(".const").textContent = constExample;
